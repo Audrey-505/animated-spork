b786edd5f0e49c37a90e05bbce084b906ce667f4
Brian Yi <brianyi244@gmail.com>
2022-12-06 21:44:09 -0500

    comboooo

--- assets/script.js
+++ assets/script.js
@@ -164,3 +164,79 @@ function goAgain(event){
     location.reload(); 
     console.log('hello')
 }
+
+
+const API_KEY = '8c0c06e88273c64c213af99ab1b69d08';
+const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=8c0c06e88273c64c213af99ab1b69d08&language=en-US`
+const IMG_URL = `https://image.tmdb.org/t/p/w500/`
+const GENRE_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=8c0c06e88273c64c213af99ab1b69d08&language=en-US`
+
+const main = document.getElementById('main')
+
+
+getMovies(API_URL)
+
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then( data => {
+            console.log(data.results);
+        showMovies(data.results);
+        })
+}
+
+function showMovies(data){
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEL = document.createElement('div');
+        movieEL.classList.add('movie');
+        movieEL.innerHTML = `<img src="${IMG_URL+poster_path}" alt="${title}">
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+  
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>`
+
+        main.append(movieEL);
+    })
+
+}
+
+function getColor(vote) {
+    if(vote> 6){
+        return 'green'
+    }else if (vote<=6 ){
+        return 'yellow'
+    }else if(vote<= 5) {
+        return 'red'
+    }
+}
+    
+
+// fetch(GENRE_URL)
+// .then(function (response){
+//     return response.json()
+// })
+// .then(function (data){
+//     //console.log(data)
+//     var genreList = data.genres
+//     var names = genreList.map(e => e.name)
+//     //console.log(names)
+//     genreMovies(names)
+// })
+
+// function genreMovies(a){
+// $(genreSelect).append(`
+//     <label for="genreSelec">Pick A Genre</label>
+//     <select class="form-control" onchange="" id="genreSelec">
+//     <option>Select A Genre</option>
+//     ${a.map(e => {
+//         //console.log(e)
+//     return `<option>${e}</option>`
+//     })}
+//  `)
+// }
