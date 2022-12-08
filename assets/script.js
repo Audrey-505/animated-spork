a33e46ce35a436154dba4e80b300e0ae086f3e65
Audrey Alcantara <adrianaquin22@gmail.com>
2022-12-08 18:48:28 -0500

    removed commented code

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,17 @@
 var movieInfo = document.getElementById('movieInfor')
 var nxtPage = document.getElementById('nxtPage')
-//var movieList = JSON.parse(localStorage.getItem('movieList'))|| []
 
 var apiKey = '8c0c06e88273c64c213af99ab1b69d08'
 
-var genreURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
-
 var results
+var title
+var overview
+var posterImg
+var released
+var filmid
+var dataArray
 
-var title;
-var overview;
-var posterImg;
-var released;
-
-var filmid;
-
-var dataArray;
-var currentData = 0
+var genreURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
 
 fetch(genreURL)
 .then(function (response){
@@ -25,21 +20,7 @@ fetch(genreURL)
 .then(function (data){
     console.log(data)
     var genreList = data.genres
-    //console.log(genreList)
-    // let array = [
-    //     {foo: 1, bar: 2},
-    //     {foo: 3, bar: 4}
-    // ]
-    //   console.log(array.map( e => e.foo ))
-    //console.log((genreList.map(e => e.name)))
-    var names = genreList.map(e => e.name)
-    var id = genreList.map(r => r.id)
-    //console.log(names)
-    //console.log(ids)
-    //genreMovies(names) WORKING
-    //return genreList
-    //genreMovies(genreList)
-    //genreId(ids)
+
     populateGenreDropdown(genreList)
 })
 
@@ -54,16 +35,12 @@ var populateGenreDropdown = (genreList) => {
     }
 };
 
-function eraseGenre(){
-    $('#genreSelect').addClass('genreHide')
-}
 
 function getGenre(){
 var pickedGenre = document.getElementById('genreSelec').value
 return pickedGenre
 }
 
-// https://api.themoviedb.org/3/discover/movie?api_key=8c0c06e88273c64c213af99ab1b69d08&language=en-US&page=10
 function goMovies(){
 var genreChoice = getGenre()
 discoverURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&with_genres=${genreChoice}`
@@ -73,18 +50,11 @@ fetch(discoverURL)
     return response.json()
 })
 .then(function (data){
-    //console.log(data)
     dataArray = data.results
-    var titles = dataArray.map(e => e.title)
-    var overview = dataArray.map(e => e.overview)
-    var releaseDate = dataArray.map(e => e.release_date)
-    var poster = dataArray.map(e => e.poster_path)
     filmid = dataArray.map(e => e.id)
     console.log(filmid)
-    //getMovie(filmid)
     getMovie(filmid)
     $('#movieInfor').html('')
-    //discoverMovies(titles, overview, releaseDate, poster)
 })
 
 function getMovie(filmId){
@@ -130,29 +100,19 @@ function goodValue(event) {
     var randomFilm = Math.floor(Math.random()*filmid.length)
     console.log('random film', filmid[randomFilm])
     results = filmid[randomFilm];
-    //var targetFilm = getMovie() 
     if (eventEl.innerText !== 'Thumbs Up'){
         $(movieInfo).html(' ')
         getMovie(results)
     } else {
         var goodFilm = $('#movieInfor')
-        //console.log(goodFilm.html())
-        //movieList.push(JSON.stringify(goodFilm.html()))
-        //localStorage.setItem('movieList', movieList)
         localStorage.setItem(results, goodFilm.html())
         $(movieInfo).html(`Added to List!<br>
         <div id="favFilmList"></div>
         <button onclick="goAgain(event)" id="againBtn">Pick A New Genre</button>`)
-        //console.log(results)
         var returnMovies = localStorage.getItem(results)
         console.log(returnMovies)
         $('#favFilmList').html(returnMovies)
         $('#btnHolder').html('')
-        // var returnMovies = JSON.parse(localStorage.getItem('movieList'))
-        // for (var i=0; i < returnMovies.length; i++){
-        //     $('#favFilmList').append(returnMovies[i])
-        // }
-        //localStorage.setItem(results, getMovie(results))
     }
     return 
     }
@@ -217,27 +177,3 @@ function getColor(vote) {
     }
 }
     
-
-// fetch(GENRE_URL)
-// .then(function (response){
-//     return response.json()
-// })
-// .then(function (data){
-//     //console.log(data)
-//     var genreList = data.genres
-//     var names = genreList.map(e => e.name)
-//     //console.log(names)
-//     genreMovies(names)
-// })
-
-// function genreMovies(a){
-// $(genreSelect).append(`
-//     <label for="genreSelec">Pick A Genre</label>
-//     <select class="form-control" onchange="" id="genreSelec">
-//     <option>Select A Genre</option>
-//     ${a.map(e => {
-//         //console.log(e)
-//     return `<option>${e}</option>`
-//     })}
-//  `)
-// }
